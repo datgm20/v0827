e0a6556079fdfb05545e3fd4f800859c7ade3533
Yu Tanaka <YRK00337@nifty.com>
2020-08-27 09:50:29 +0900

    完成

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,9 +12,30 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            // 2次元クラスPoint型の変数mpを宣言
+            Point mp = MousePosition;
+
+            // mpに、マウスのフォーム座標を取り出す。
+            //// MousePositionにマウス座標のスクリーン左上からのX、Yが入っている。
+            //// PointToClient()を使うと、スクリーン座標を、フォーム座標に変換できる。
+            mp = PointToClient(mp);
+
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、mp.X、mp.Yで取り出せる。
+            label2.Text = "" + mp.X + "," + mp.Y;
         }
     }
 }
