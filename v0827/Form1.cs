230c6898d481549faac7c9684fc59f7e6dc2ed78
Yu Tanaka <YRK00337@nifty.com>
2020-08-27 12:22:17 +0900

    3つ

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,8 +12,12 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
-        int vx = rand.Next(-10,11);
-        int vy = rand.Next(-10,11);
+        int vx0 = rand.Next(-20,21);
+        int vy0 = rand.Next(-20,21);
+        int vx1 = rand.Next(-20, 21);
+        int vy1 = rand.Next(-20, 21);
+        int vx2 = rand.Next(-20, 21);
+        int vy2 = rand.Next(-20, 21);
         int point = 100;
         static Random rand = new Random();
 
@@ -23,6 +27,10 @@ namespace v0827
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label4.Left = rand.Next(ClientSize.Width - label4.Width);
+            label4.Top = rand.Next(ClientSize.Height - label4.Height);
+            label5.Left = rand.Next(ClientSize.Width - label5.Width);
+            label5.Top = rand.Next(ClientSize.Height - label5.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -30,24 +38,62 @@ namespace v0827
             point--;
             label3.Text = "Score " + point;
             
-            label1.Left += vx;
-            label1.Top += vy;
+            label1.Left += vx0;
+            label1.Top += vy0;
+            label4.Left += vx1;
+            label4.Top += vy1;
+            label5.Left += vx2;
+            label5.Top += vy2;
 
             if (label1.Left < 0)
             {
-                vx = Math.Abs(vx);
+                vx0 = Math.Abs(vx0);
             }
             if (label1.Top < 0)
             {
-                vy = Math.Abs(vy);
+                vy0 = Math.Abs(vy0);
             }
             if (label1.Right > ClientSize.Width)
             {
-                vx = -Math.Abs(vx);
+                vx0 = -Math.Abs(vx0);
             }
             if (label1.Bottom > ClientSize.Height)
             {
-                vy = -Math.Abs(vy);
+                vy0 = -Math.Abs(vy0);
+            }
+
+            if (label4.Left < 0)
+            {
+                vx1 = Math.Abs(vx1);
+            }
+            if (label4.Top < 0)
+            {
+                vy1 = Math.Abs(vy1);
+            }
+            if (label4.Right > ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx1);
+            }
+            if (label4.Bottom > ClientSize.Height)
+            {
+                vy1 = -Math.Abs(vy1);
+            }
+
+            if (label5.Left < 0)
+            {
+                vx2 = Math.Abs(vx2);
+            }
+            if (label5.Top < 0)
+            {
+                vy2 = Math.Abs(vy2);
+            }
+            if (label5.Right > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2);
+            }
+            if (label5.Bottom > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy2);
             }
 
             // 2次元クラスPoint型の変数mpを宣言
