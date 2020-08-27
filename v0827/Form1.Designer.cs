e08a25cd48d733deac70d9b1fa692578a186d7d5
Yu Tanaka <YRK00337@nifty.com>
2020-08-27 12:12:11 +0900

    12-2

--- v0827/Form1.Designer.cs
+++ v0827/Form1.Designer.cs
@@ -39,7 +39,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(113, 104);
+            this.label1.Location = new System.Drawing.Point(282, 162);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(135, 48);
             this.label1.TabIndex = 0;
@@ -73,7 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 352);
+            this.ClientSize = new System.Drawing.Size(843, 407);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
